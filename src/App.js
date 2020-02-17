2510f86ac507c4c024431d1d566aca07b94c4ddf
Pritam Khose <pritamkhose@gmail.com>
2020-02-17 11:16:33 +0000

    lesson-7-react-link

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 import "./App.css";
+
+// https://github.com/ZhangMYihua/lesson-7/blob/master/src/App.js
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+          <Header />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/shop" component={ShopPage} />
           </Switch>
-        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.styles.scss";
+
+//port SHOP_DATA from "../../pages/shop/shop.data.js";
+
+// import CollectionPreview from "../../components/collection-preview/collection-preview";
+// // <Logo className="logo" />
+// https://stackoverflow.com/questions/42296499/how-to-display-svg-icons-svg-files-in-ui-using-react-component
+
+class Header extends React.Component {
+  render() {
+    return (
+      // const Header = () => (
+      <div className="header">
+        <Link className="logo-container" to="/">
+          <img className="logo" alt="Logo" src={require("../../assets/crown.svg")} />
+        </Link>
+        <div className="options">
+          <Link className="option" to="/shop">
+            SHOP
+          </Link>
+          <Link className="option" to="/shop">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
