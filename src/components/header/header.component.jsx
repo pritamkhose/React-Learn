58a6247057a92b2654a0c1fe81fc91860f1b018f
pritam <pritamkhose@gmail.com>
2020-02-18 00:40:28 +0530

    lesson-12-16-redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { auth } from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 // import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
@@ -12,31 +16,42 @@ import "./header.styles.scss";
 // class Header extends React.Component {
 //   render() {
 //     return (
-      const Header = ({ currentUser }) => (
-      <div className="header">
-        <Link className="logo-container" to="/">
-          <img className="logo" alt="Logo" src={require("../../assets/crown.svg")} />
-        </Link>
-        <div className="options">
-          <Link className="option" to="/shop">
-            SHOP
-          </Link>
-      <Link className='option' to='/shop'>
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <img
+        className="logo"
+        alt="Logo"
+        src={require("../../assets/crown.svg")}
+      />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'>
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
       )}
-        </div>
-      </div>
-    );
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
+);
 //   }
 // }
 
-export default Header;
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
+
+export default connect(mapStateToProps)(Header);
